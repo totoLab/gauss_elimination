6e6d91d48856911416ef4f662c9e96f59e193d8c
toto <antoniolabate19@gmail.com>
2021-12-30 23:13:51 +0100

    mod 'all zeros' exit condition,cleaning+refactoring

--- gauss_elimination.py
+++ gauss_elimination.py
@@ -12,25 +12,22 @@ def main(matrix):
 
     return matrix, row_switches, pivot_list
         
+def print_and_exit(matrix, row_switches, pivot_list):
+    ulm.stampa_matrice_incolonnata(matrix, 5)
+    return row_switches, pivot_list
 
 def flow(matrix, pivot_list, row_switches):
     ulm.stampa_matrice_incolonnata(matrix, 5)
     print()
     pivot, row_switches = gauss.search_pivot(matrix, pivot_list, row_switches) # tuples unpacking
     pivot_list.append(pivot)
-    if gauss.last_possible_pivot(matrix, pivot) or gauss.last_null_rows(matrix, pivot):
-        print(pivot_list)
-        return row_switches, pivot_list
+    if gauss.last_possible_pivot(matrix, pivot):
+        return print_and_exit(matrix, row_switches, pivot_list)
     else:
         if not gauss.exists_null_sub_column(matrix, pivot):
             gauss.generate_new_row(matrix, pivot)
             
-        return flow(matrix, pivot_list, row_switches)
+        if gauss.last_null_rows(matrix, pivot):
+            return print_and_exit(matrix, row_switches, pivot_list)
 
-# example
-# matrix = [
-#     [0, 1/2,-1, 0],
-#     [0, 1, 1/2, 2],
-#     [4, 5, 4, 0.3333]
-# ]
-# gauss_elimination(matrix)
+        return flow(matrix, pivot_list, row_switches)
