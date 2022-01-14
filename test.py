28a169c3686c0146919a88839c6f93346c8593ef
toto <antoniolabate19@gmail.com>
2022-01-14 22:38:38 +0100

    added functions for testing and a little UI

--- test.py
+++ test.py
@@ -1,36 +1,43 @@
-import gauss_elimination
-import ulm
-import determinant
 import random
+import ulm
+import gauss_elimination
+import determinant
 
-def verify_results():
-    results = {'equal': 0, 'different': 0}
-    for i in range(10):
-        matrix = ulm.costruisci_matrice_valori_casuali(10,10,305239463467)
-        #ulm.stampa_matrice(matrix)
-        laplace = determinant.laplace_expansion(matrix)
-        diagonal = determinant.product_determinant(matrix)
-
-        if laplace == diagonal:
-            results['uguali'] += 1
-        elif laplace != diagonal:
-            results['diversi'] += 1
-        else:
-            print('errore')
-
-    print(results)
-
-def test():
-    max_value = 50000
-
+def matrix_input(max_value):
     choices = ["Matrice inserita dall'utente", "Matrice casuale"]
     choice = determinant.cli_UI(choices, "Numero corrispondente al tipo di matrice: ")
     if choice == 0:
         matrix = ulm.leggi_matrice_int()
     elif choice == 1:
-        matrix = ulm.costruisci_matrice_valori_casuali(random.randint(1, 15), random.randint(1, 15), max_value)
+        matrix = ulm.costruisci_matrice_valori_casuali(random.randint(1, 10), random.randint(1, 10), max_value)
 
-    ulm.stampa_matrice_incolonnata(matrix, len(str(max_value)))
+    return matrix
+
+def determinant_test(matrix, max_length):
+    ulm.stampa_matrice_incolonnata(matrix, max_length)
     print("Determinante:", determinant.main(matrix))
 
-test()
+def gauss_elimination_test(matrix, max_length):
+    print("Matrice da ridurre")
+    ulm.stampa_matrice_incolonnata(matrix, max_length)
+
+    reduced_matrix, _, _ = gauss_elimination.main(matrix)
+    print("Matrice ridotta")
+    ulm.stampa_matrice_incolonnata(reduced_matrix, max_length)
+
+def general_test():
+    element_max_value = 500
+    max_element_length = len(str(element_max_value))
+
+    operations = ["Determinante", "Riduzione di Gauss"]
+    label = "Inserire il numero corrispondente all'operazione da svolgere: "
+    choice = determinant.cli_UI(operations, label)
+
+    matrix = matrix_input(element_max_value)
+
+    if choice == 0:
+        determinant_test(matrix, max_element_length)
+    elif choice == 1:
+        gauss_elimination_test(matrix, max_element_length)
+
+general_test()
